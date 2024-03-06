eaf84414b6ce607bdeee7755f2d64865196c4c8f
4mkbs <mkb4sakib@gmail.com>
2024-03-06 16:29:07 +0600

    lws tutorial code for good code or clean code practices

--- index.js
+++ index.js
@@ -1,78 +1,46 @@
-/* this is a one part of the code(1st part)
-const { log } = require('console');
-const fs= require('fs');
+/*
+ * Title: Basic Node app example
+ * Description: Simple node application that print random quotes per second interval.
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 11/09/19
+ *
+ */
 
-const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`,'utf8');
+// Dependencies
+const mathLibrary = require('./lib/math');
+const quotesLibrary = require('./lib/quotes');
 
-ourReadStream.on('data',(data)=>{
-    // console.log(chunk.toString());
-    // console.log(data);
-});
-log('Reading file...');
+// App object - Module scaffolding
+const app = {};
 
-*/
+// Configuration
+app.config = {
+    timeBetweenQuotes: 1000,
+};
 
-/* this is a another part of the code (2nd part)
+// Function that prints a random quote
+app.printAQuote = function printAQuote() {
+    // Get all the quotes
+    const allQuotes = quotesLibrary.allQuotes();
 
-const http = require('http');
+    // Get the length of the quotes
+    const numberOfQuotes = allQuotes.length;
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.write(`
-        <html>
-        <head>
-        <title>My First Node App</title>
-        </head>
-        <body>
-        <h1>Welcome to my first Node App</h1>
-        <form action="/about" method="POST">
-        <input type="text" value="sakib" name="recieved">
-        <form>
-        </body>
-        </html>
-        `);
-        res.end();
-    }else if(req.url === '/about' && req.method === 'POST'){
-        req.on('data',(chunk)=>{
-            console.log(chunk.toString());
-        });
-        res.write('<h1>About Page</h1>');
-        console.log('About Page');
-        res.end();
-    }else{   
-        res.write('Not Found');
-        res.end();
-    }
-});
+    // Pick a random number between 1 and the number of quotes
+    const randomNumber = mathLibrary.getRandomNumber(1, numberOfQuotes);
 
-server.listen(3000);
-console.log('Server is listening on port 3000...');
-*/
+    // Get the quote at that position in the array (minus one)
+    const selectedQuote = allQuotes[randomNumber - 1];
 
+    // Print the quote to the console
+    console.log(selectedQuote);
+};
 
-/* this is a another part of the code (3rd part)
+// Function that loops indefinitely, calling the printAQuote function as it goes
+app.indefiniteLoop = function indefiniteLoop() {
+    // Create the interval, using the config variable defined above
+    setInterval(app.printAQuote, app.config.timeBetweenQuotes);
+};
 
-const { log } = require('console');
-const fs= require('fs');
-
-const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
-const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
-
-// ourReadStream.on('data',(chunk)=>{
-//    ourWriteStream.write(chunk);
-// });
-ourReadStream.pipe(ourWriteStream);
-log('writing file...');
-
-*/
-
-// this is a another part of the code (4th part)
-
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res)=>{
-    const myReadstream = fs.createReadStream(`${__dirname}/bigData.txt`,'utf8');
-    myReadstream.pipe(res);
-});
-server.listen(3000);
-console.log('Server is listening on port 3000...');
+// Invoke the loop
+app.indefiniteLoop();
